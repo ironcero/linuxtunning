28ca9e518a8344284c912d623267a93dfc781d91
ironcero <ironcero@gmail.com>
2017-11-29 23:50:13 +0100

    Create properties.sh

---
+++ properties.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+INSTALLED_SUCCESS_TEXT="install ok installed"
+BASE_RESPOSITORY=https://github.com/ironcero/linuxtunning
+DIRECT_MASTER_DOWNLOAD_URL=$BASE_RESPOSITORY/blob/master
