daf9913043ab2a56297b3e64508a492e058d13ff
ironcero <ironcero@gmail.com>
2017-11-29 23:43:59 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
-INSTALLED_SUCCESS_TEXT="install ok installed"
+#INSTALLED_SUCCESS_TEXT="install ok installed"
 #Vim
 echo "Checking if Git is installed..."
-if [ -z `dpkg -s git | grep $INSTALLED_SUCCESS_TEXT` ]; then
+if [ -z `dpkg -s git | grep "$INSTALLED_SUCCESS_TEXT"` ]; then
     apt-get install -y git
-    if [ -z `dpkg -s git | grep $INSTALLED_SUCCESS_TEXT` ]; then
+    if [ -z `dpkg -s git | grep "$INSTALLED_SUCCESS_TEXT"` ]; then
         echo "Git failed to install"
         exit 1
     else
