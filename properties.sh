e3d1663a1cf2e823fd632d3aa29faa11175a06f3
ironcero <ironcero@gmail.com>
2017-11-30 22:50:53 +0100

    Update properties.sh

--- properties.sh
+++ properties.sh
@@ -2,4 +2,4 @@
 
 export INSTALLED_SUCCESS_TEXT="install ok installed"
 export BASE_RESPOSITORY=https://github.com/ironcero/linuxtunning
-export DIRECT_MASTER_DOWNLOAD_URL=$BASE_RESPOSITORY/blob/master
+export DIRECT_MASTER_DOWNLOAD_URL=$BASE_RESPOSITORY/raw/master
