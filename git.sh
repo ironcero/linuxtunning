4b39f8fa1750a95d000c0576e2147af963e8d84a
ironcero <ironcero@gmail.com>
2017-11-30 23:05:32 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@ git_installed=`dpkg -s git | grep "$INSTALLED_SUCCESS_TEXT"`
 if [ -z "$git_installed" ]; then
     apt-get install -y git
     git_installed=`dpkg -s git | grep "$INSTALLED_SUCCESS_TEXT"`
-    if [ -z "$git_installed" ]; then
+    if [ -n "$git_installed" ]; then
         echo "Git failed to install"
         exit 1
     else
